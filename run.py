572f91c78e673c02dc427994a0f392aa3b876ed1
HUGH <445073309@qq.com>
2017-10-18 01:08:27 +0800

    init simple framework

---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import (
+    Flask,
+    abort,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'import_thing'
+
+@app.route('/')
+def show_posts():
+    return render_template('/index.html')
+
+if __name__ == '__main__':
+    app.run()
