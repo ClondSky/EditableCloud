d40f9aae4c7fc747993a73cb6c2e932cf3377afb
HUGH <445073309@qq.com>
2017-10-21 18:52:11 +0800

    init branch

--- templates/PO/__init__.py
+++ templates/PO/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-sql_db = create_engine("sqlite:///TYCloud.db", echo=True)
+basedir = os.path.abspath("templates")  # 获得根目录
+sqlpath = "sqlite:///" + basedir + "\\TYCloud.db"
+print(sqlpath)
+sql_db = create_engine("sqlite+pysqlite:///L:/TYcloud/EditableCloud/templates/oosApi/templates/TYCloud.db", echo=True)
 DB_session = sessionmaker(bind=sql_db)
